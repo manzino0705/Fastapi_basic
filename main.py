56ffbcd15728cadbd5b6965ddce2e7db3a047e69
manzino0705 <53653584+manzino0705@users.noreply.github.com>
2023-01-30 11:41:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("my name is MINJI")
+# Text elements
+st.title("💰한국투자증권 신입사원 **:red[DT]** 프로젝트")
+st.header("Streamlit 프로토타입 만들기")
+st.subheader("[Streamlit] 활용하기")
+st.markdown("***'kim min ji web application'***\n\n")
